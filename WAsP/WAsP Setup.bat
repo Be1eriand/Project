617cf38f9b37242183898ad0090ac44e74a8a14d
Be1eriand <e_meiss@hotmail.com>
2022-03-31 19:11:07 +1030

    corrected wasp_set-up.bat

--- WAsP/WAsP Setup.bat
+++
@@ -1,8 +0,0 @@
-@echo Creating Virtual Environment for WAsP Backend
-cmd /C python -m venv venv
-
-@echo Changing to Virtual Environment
-cmd /C .\venv\Scripts\Activate.ps1
-
-@echo Installing required python modules
-cmd /C pip install -r requirements.txt
---
+++ WAsP/WAsP_Setup.bat
@@ -0,0 +1,8 @@
+@echo Creating Virtual Environment for WAsP Backend
+cmd /C python -m venv venv
+
+::@echo Changing to Virtual Environment
+::.\venv\Scripts\Activate.ps1
+
+::@echo Installing required python modules
+::cmd /C pip install -r requirements.txt
