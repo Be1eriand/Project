905ce908f85d4855630ed916ec0a64f862dc26df
Be1eriand <e_meiss@hotmail.com>
2022-06-02 18:53:21 +0930

    update

---
+++ WAsP/WAsP Start.ps1
@@ -0,0 +1,5 @@
+Start-Process -FilePath  "python" -ArgumentList ".\RealTimeInterface\RealTimeInterface.py"
+
+cd .\backend\wasp
+
+Start-Process -FilePath  "python" -ArgumentList ".\RealTimeInterface\RealTimeInterface.py"
