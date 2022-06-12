a77506b8ad00fded7f33f30b150b5efe3084745c
Karla Mc <kammac802@gmail.com>
2022-06-12 23:25:22 +1000

    Home page and login completed

--- WAsP/Frontend/WAsP/src/app/home/home.component.ts
+++ WAsP/Frontend/WAsP/src/app/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent {
         private router: Router) {
         this.user = this.accountService.userValue;
     }
-    logout() {
+   logout() {
         this.accountService.logout();
     }
 }
